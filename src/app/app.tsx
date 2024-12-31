b112721c743a31b0b2306b8d18de561fab9c002d
zim89 <121241841+zim89@users.noreply.github.com>
2024-12-31 15:12:37 +0200

    #01 - add routing

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,12 @@
-fetch('http://localhost:4000/users').then(response => {
-  console.log(response)
-})
+import { Route, Routes } from 'react-router-dom'
+import { UsersPage } from '../pages/users'
+import { TodoListPage } from '../pages/todo-list'
 
 export function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return (
+    <Routes>
+      <Route path='/' element={<UsersPage />}></Route>
+      <Route path='/:userId/tasks' element={<TodoListPage />}></Route>
+    </Routes>
+  )
 }
