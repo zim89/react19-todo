8429632d978b96c19debb7884cb3de7585fbda68
zim89 <121241841+zim89@users.noreply.github.com>
2025-01-01 13:10:44 +0200

    #3 - Pessimistic Updates
    add Pessimistic Updates using useTransition hook

--- src/pages/users/page.tsx
+++ src/pages/users/page.tsx
@@ -1,14 +1,15 @@
 import { fetchUsers, type User } from '@/shared/api'
 import { CreateUserForm } from './ui/create-user-form'
 import { UsersList } from './ui/user-list'
-import { Suspense, useState } from 'react'
+import { startTransition, Suspense, useState } from 'react'
 
 const defaultUsersPromise = fetchUsers()
 
 export function UsersPage() {
   const [usersPromise, setUsersPromise] =
     useState<Promise<User[]>>(defaultUsersPromise)
-  const refetchUsers = () => setUsersPromise(fetchUsers())
+  const refetchUsers = () =>
+    startTransition(() => setUsersPromise(fetchUsers()))
 
   return (
     <main className='container mx-auto space-y-6 p-4 pt-10'>
@@ -16,7 +17,7 @@ export function UsersPage() {
       <CreateUserForm refetchUsers={refetchUsers} />
 
       <Suspense fallback={<div>Loading users...</div>}>
-        <UsersList usersPromise={usersPromise} />
+        <UsersList usersPromise={usersPromise} refetchUsers={refetchUsers} />
       </Suspense>
     </main>
   )
--- src/pages/users/ui/create-user-form.tsx
+++ src/pages/users/ui/create-user-form.tsx
@@ -1,27 +1,32 @@
 import { createUser } from '@/shared/api'
-import { useState } from 'react'
+import { useState, useTransition } from 'react'
 
 export function CreateUserForm({ refetchUsers }: { refetchUsers: () => void }) {
   const [email, setEmail] = useState('')
+  const [isPending, startTransition] = useTransition()
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
-    await createUser({ email, id: crypto.randomUUID() })
-    refetchUsers()
-    setEmail('')
+    startTransition(async () => {
+      await createUser({ email, id: crypto.randomUUID() })
+      refetchUsers()
+      setEmail('')
+    })
   }
 
   return (
     <form onSubmit={handleSubmit} className='flex gap-2'>
       <input
         value={email}
+        disabled={isPending}
         onChange={e => setEmail(e.target.value)}
         type='email'
         className='rounded-md border p-2'
       />
       <button
+        disabled={isPending}
         type='submit'
-        className='rounded-md bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700'
+        className='rounded-md bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 disabled:bg-gray-400'
       >
         Add
       </button>
