e2df37be01c8013121c8052d72b6ffcb6177c589
zim89 <121241841+zim89@users.noreply.github.com>
2025-01-01 14:28:21 +0200

    #4 - error boundary and useActionState

--- src/pages/users/page.tsx
+++ src/pages/users/page.tsx
@@ -1,24 +1,28 @@
-import { fetchUsers, type User } from '@/shared/api'
 import { CreateUserForm } from './ui/create-user-form'
 import { UsersList } from './ui/user-list'
-import { startTransition, Suspense, useState } from 'react'
-
-const defaultUsersPromise = fetchUsers()
+import { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+import { useUsers } from './use-users'
 
 export function UsersPage() {
-  const [usersPromise, setUsersPromise] =
-    useState<Promise<User[]>>(defaultUsersPromise)
-  const refetchUsers = () =>
-    startTransition(() => setUsersPromise(fetchUsers()))
+  const [usersPromise, refetchUsers] = useUsers()
 
   return (
     <main className='container mx-auto space-y-6 p-4 pt-10'>
       <h1 className='text-3xl font-bold'>Users</h1>
       <CreateUserForm refetchUsers={refetchUsers} />
 
-      <Suspense fallback={<div>Loading users...</div>}>
-        <UsersList usersPromise={usersPromise} refetchUsers={refetchUsers} />
-      </Suspense>
+      <ErrorBoundary
+        fallbackRender={e => (
+          <div className='text-red-500'>
+            Something went wrong: {JSON.stringify(e)}
+          </div>
+        )}
+      >
+        <Suspense fallback={<div>Loading users...</div>}>
+          <UsersList usersPromise={usersPromise} refetchUsers={refetchUsers} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   )
 }
